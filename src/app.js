d78819a55b9132a9f06e0d852d04a1ca600a5a5e
sina <sina@sinas-MacBook-Pro.local>
2022-06-19 12:17:59 +0430

    adding postgress sql url and sequlize to as ORM

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import bodyParser from "body-parser";
 import express from "express";
 import 'dotenv/config'
 
+import { Sequelize } from "sequelize";
+
+const sequlize = new Sequelize(process.env.postgresURL)
+
 
 const app = express()
 
@@ -13,7 +17,14 @@ app.use(bodyParser.json())
 
 
 
+
 app.listen(PORT, () => {
+    try {
+        sequlize.authenticate();
+        console.log(`connected to database on url: ${process.env.postgresURL}`);
+    } catch (error) {
+        console.error(error);
+    }
     console.log(`server is running on port: ${PORT}`)
 })
 
